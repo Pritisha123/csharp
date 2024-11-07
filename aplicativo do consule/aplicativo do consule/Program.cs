35ebf63bc91ee1e22ff8c9f2a174aad8e46380b4
Pritisha123 <adhikaripritisa926@gmail.com>
2024-11-07 12:14:40 +0000

    Add files via upload

---
+++ aplicativo do consule/aplicativo do consule/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class N4
+{
+
+  public static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 5;
+        int c = 10;
+        int d = 2;
+
+        if (a == b && b == c && c == d)
+        {
+
+
+            Console.WriteLine("Is this Numbers are equal");
+
+        }
+        else
+        {
+            Console.WriteLine();
+
+
+        }
+  }
+
+
+}
+         
+
+
